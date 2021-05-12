2b8cbcf8037ddcaa55749c7c1a1f710ed326ab0c
vatana7 <vatanamain@gmail.com>
2021-05-13 00:12:15 +0700

    12

--- userprofile/views.py
+++ userprofile/views.py
@@ -16,19 +16,19 @@ from .models import Userprofile
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        
+
         if form.is_valid():
             user = form.save()
             user.email = user.username
             user.save()
-            
+
             userprofile = Userprofile.objects.create(user=user)
-            
+
             login(request, user)
-            
-            return redirect('frontpage')
+
+            return redirect('trackhours:frontpage')
     else:
         form = UserCreationForm()
-        
+
     return render(request, 'userprofile/signup.html', {'form': form})
             
