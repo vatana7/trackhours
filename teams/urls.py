b44768e8bf4e2c28ea75c41a87c5bbf5233b7bc2
vatana7 <vatanamain@gmail.com>
2021-06-01 17:06:06 +0700

    team activate

--- teams/urls.py
+++ teams/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import add
+from .views import add, team, edit, activate_team
 
 app_name = 'teams'
 
 urlpatterns = [
     path('add/', add, name='add'),
+    path('<int:team_id>/', team, name='team'),
+    path('edit/', edit, name='edit'),
+    path('activate_team/<int:team_id>/', activate_team, name='activate_team')
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from userprofile.models import Userprofile
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 
 
@@ -24,3 +25,39 @@ def add(request):
             return redirect('myaccount')
     
     return render(request, 'teams/add.html')
+
+@login_required
+def team(request, team_id):
+    team = get_object_or_404(Team, pk=team_id, status=Team.ACTIVE, members__in=[request.user])
+
+    return render(request, 'teams/team.html', {'team': team})
+
+@login_required
+def edit(request):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE, members__in=[request.user])
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+
+        if title:
+            team.title = title
+            team.save()
+
+            messages.info(request, 'The changes was saved')
+
+            return redirect('teams:team', team_id=team.id)
+    
+    return render(request, 'teams/edit.html', {'team': team})
+
+@login_required
+def activate_team(request, team_id):
+    team = get_object_or_404(Team, pk=team_id, status=Team.ACTIVE, members__in=[request.user])
+    userprofile = request.user.userprofile
+    userprofile.active_team_id = team.id
+    userprofile.save()
+
+    messages.info(request, 'The team was activated')
+
+    return redirect('teams:team', team_id=team.id)
+
+    
